8df26fae151491b2c0ca027c9b4540637cbfff53
ayeo <me@ayeo.pl>
2015-10-16 00:28:31 +0200

    Initial

---
+++ src/Price.php
@@ -0,0 +1,296 @@
+<?php
+namespace Ayeo\Price;
+
+/**
+ * Price model
+ */
+class Price
+{
+    /**
+     * todo: Add map symbol => precision
+     * There exists currencies with different precision
+     * but those are extremely uncommon
+     *
+     * Full list:
+     * https://pl.wikipedia.org/wiki/Jen
+     * https://pl.wikipedia.org/wiki/Funt_cypryjski
+     * https://pl.wikipedia.org/wiki/Dinar_iracki
+     * https://pl.wikipedia.org/wiki/Dinar_jordański
+     * https://pl.wikipedia.org/wiki/Dinar_kuwejcki
+     * https://pl.wikipedia.org/wiki/Dinar_Bahrajnu
+     */
+    const PRECISION = 2;
+
+    /**
+     * @var float
+     */
+	private $nett;
+
+    /**
+     * @var float
+     */
+	private $gross;
+
+    /**
+     * @var null|string ISO 4217 (3 uppercase chars)
+     */
+	private $currencySymbol;
+
+    /**
+     * @param float $nett
+     * @param float $gross
+     * @param null $currencySymbol
+     */
+	public function __construct($nett = 0.00, $gross = 0.00, $currencySymbol = null)
+	{
+        $this->validateValues($nett, $gross);
+        $this->currencySymbol = $this->processCurrencySymbol($currencySymbol);
+
+		$this->nett = $nett;
+		$this->gross = $gross;
+	}
+
+    /**
+     * @param float $nett
+     * @param integer $tax
+     * @return Price
+     */
+	static public function buildByNett($nett, $tax)
+	{
+        //todo: validate tax is integer
+		$gross = $nett * (100 + $tax) / 100;
+		return new Price($nett, $gross); //todo:tests!
+	}
+
+    //todo: buildByGross
+
+    /**
+     * @return float
+     */
+	public function getGross()
+	{
+		return round($this->gross, Price::PRECISION);
+	}
+
+    /**
+     * @return float
+     */
+	public function getNett()
+	{
+		return round($this->nett, Price::PRECISION);
+	}
+
+    /**
+     * @return int
+     */
+	public function getTax()
+	{
+        if ($this->nett > 0)
+        {
+            return round($this->gross / $this->nett * 100 - 100, 0);
+        }
+
+        return 0;
+	}
+
+    /**
+     * Returns 3 chars iso 4217 symbol
+     * @return string
+     */
+    public function getCurrencySymbol()
+    {
+        if (is_null($this->currencySymbol))
+        {
+            throw new \RuntimeException('Currency symbol is not set');
+        }
+
+        return $this->currencySymbol;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasCurrency()
+    {
+        return isset($this->currencySymbol);
+    }
+
+    /**
+     * @param Price $price
+     * @return bool
+     */
+    public function isGreaterThan(Price $price)
+    {
+        return $this->getGross() > $price->getGross();
+    }
+
+    /**
+     * @param Price $price
+     * @return bool
+     */
+    public function isLowerThan(Price $price)
+    {
+        return $this->getGross() < $price->getGross();
+    }
+
+    /**
+     * @param Price $price
+     * @return bool
+     */
+    public function isEqual(Price $price)
+    {
+        $isGrossEqual = $this->getGross() === $price->getGross();
+        $isNettEqual = $this->getNett() === $price->getNett();
+
+        return ($isGrossEqual  && $isNettEqual);
+    }
+
+    /**
+     * @param Price $priceToAdd
+     * @return Price
+     */
+	public function add(Price $priceToAdd)
+	{
+		$this->checkCurrencies($this, $priceToAdd);
+
+		$newGross = $this->getGross() + $priceToAdd->getGross();
+		$newNett = $this->getNett() + $priceToAdd->getNett();
+
+		return new Price($newNett, $newGross, $this->currencySymbol);
+	}
+
+    /**
+     * @param Price $priceToSubtract
+     * @return Price
+     */
+    public function subtract(Price $priceToSubtract)
+    {
+        $this->checkCurrencies($this, $priceToSubtract);
+
+        if ($this->isGreaterThan($priceToSubtract))
+        {
+            $newGross = $this->getGross() - $priceToSubtract->getGross();
+            $newNett = $this->getNett() - $priceToSubtract->getNett();
+
+            return new Price($newNett, $newGross, $this->currencySymbol);
+        }
+
+        return new Price(); //zero
+    }
+
+    /**
+     * @param integer $times
+     * @return Price
+     */
+    public function multiply($times)
+    {
+        //todo: times must be integer!
+        //todo: test zero
+        $nett = $this->getNett() * $times;
+        $gross = $this->getGross() * $times;
+
+        return new Price($nett, $gross, $this->currencySymbol);
+    }
+
+    /**
+     * @param float $gross
+     * @return Price
+     */
+    public function subtractGross($gross)
+    {
+        //todo: must be numeric
+		//todo: gross must be positive!
+        //todo: must be greater than zero
+
+        if ($gross > $this->getGross())
+        {
+            return new Price();
+        }
+
+        $newGross = $this->getGross() - (float) $gross;
+        $newNett = $newGross / (1 + $this->getTax() / 100);
+
+        return new Price($newNett, $newGross, $this->currencySymbol);
+    }
+
+    //todo: addGross()
+
+    /**
+     * @param Price $A
+     * @param Price $B
+     */
+    private function checkCurrencies(Price $A, Price $B)
+    {
+        if ($A->hasCurrency() === false && $B->hasCurrency() === false)
+        {
+            return;
+        }
+
+        //fixme: one of currencies may still not been set here
+        //we get exception anyway, should we translate the exception here?
+
+        if ($A->getCurrencySymbol() !== $B->getCurrencySymbol())
+        {
+            $message = sprintf(
+                'Can not do operate on different currencies ("%s" and "%s")',
+                $A->getCurrencySymbol(),
+                $B->getCurrencySymbol()
+            );
+            throw new \LogicException($message);
+        }
+
+    }
+
+    /**
+     * @param float $nett
+     * @param float $gross
+     */
+    private function validateValues($nett, $gross)
+    {
+        if (is_numeric($nett) === false)
+        {
+            throw new \LogicException('Nett must be numeric');
+        }
+
+        if (is_numeric($gross) === false)
+        {
+            throw new \LogicException('Gross must be numeric');
+        }
+
+        if ($nett < 0)
+        {
+            throw new \LogicException('Nett must be positive');
+        }
+
+        if ($gross < 0)
+        {
+            throw new \LogicException('Gross must be positive');
+        }
+
+        if ($nett > $gross)
+        {
+            throw new \LogicException('Nett must not be greater than gross');
+        }
+    }
+
+    /**
+     * @param null|string $currencySymbol ISO 4217 (3 uppercase chars)
+     * @return string
+     */
+    private function processCurrencySymbol($currencySymbol = null)
+    {
+        if (is_null($currencySymbol) === false)
+        {
+            //ISO 4217 (3 uppercase chars)
+            if (preg_match('#^[A-Z]{3}$#', $currencySymbol))
+            {
+                return strtoupper($currencySymbol);
+            }
+            else
+            {
+                $message = sprintf('Invalid currency symbol: "%s"', $currencySymbol);
+                throw new \LogicException($message);
+            }
+        }
+    }
+}
---
+++ tests/PriceTest.php
@@ -0,0 +1,189 @@
+<?php
+namespace Ayeo\Price\Test;
+
+use Ayeo\Price\Price;
+use LogicException;
+
+class PriceTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @dataProvider testCreatingDataProvider
+     */
+	public function testCreating($nett, $gross, $tax)
+	{
+		$price = new Price($nett, $gross);
+        $this->assertEquals($tax, $price->getTax());
+	}
+
+    public function testCreatingDataProvider()
+    {
+        return [
+            [100, 123, 23],
+            [68.2927, 84.0000, 23],
+            [31.7073, 39.0000, 23],
+
+            [109.7561, 135.0000, 23],
+            [109.7561, 135.0000, 23],
+            [109.7561, 135.0001, 23],
+            [109.7561, 135.0002, 23],
+            [109.7561, 135.0003, 23],
+            [109.7561, 135.0004, 23],
+            [109.7561, 135.0005, 23],
+            [109.7561, 135.0006, 23],
+            [109.7561, 135.0007, 23],
+            [109.7561, 135.0008, 23],
+            [109.7561, 135.0009, 23],
+            [109.7561, 135.0010, 23],
+            [109.7561, 135.0020, 23],
+            [109.7561, 135.0030, 23],
+            [109.7561, 135.0040, 23],
+            [109.7561, 135.0050, 23],
+            [109.7561, 135.0060, 23],
+            [109.7561, 135.0070, 23],
+            [109.7561, 135.0100, 23],
+            [109.7561, 135.5000, 23],
+        ];
+    }
+
+	/**
+	 * @dataProvider testAddingDataProvider
+	 */
+	public function testAdding($grossA, $grossB, $expectedGross)
+	{
+        $tax = 23;
+		$nettA = $grossA / (100 + $tax) * 100;
+		$nettB = $grossB / (100 + $tax) * 100;
+
+		$A = new Price($nettA, $grossA);
+		$B = new Price($nettB, $grossB);
+
+		$this->assertEquals($expectedGross, $A->add($B)->getGross());
+		$this->assertEquals($expectedGross, $B->add($A)->getGross());
+
+		$this->assertEquals($tax, $B->add($A)->getTax());
+		$this->assertEquals($tax, $A->add($B)->getTax());
+
+		$this->assertEquals($nettA, $A->getNett(), '', 0.01);
+		$this->assertEquals($nettB, $B->getNett(), '', 0.01);
+	}
+
+	public function testAddingDataProvider()
+	{
+		return [
+			[123.00,     246.00,    369.00],
+			[ 32.21,      33.32,     65.53],
+		];
+	}
+
+    public function testPricesAreImmutableWhileAdding()
+    {
+        $A = new Price(100, 120, 'PLN');
+        $B = new Price(200, 300, 'PLN');
+        $A->add($B);
+
+        $this->assertEquals(100, $A->getNett());
+        $this->assertEquals(120, $A->getGross());
+
+        $this->assertEquals(200, $B->getNett());
+        $this->assertEquals(300, $B->getGross());
+    }
+
+    public function testAddingSameCurrencies()
+    {
+        $A = new Price(100, 130, 'USD');
+        $B = new Price(300, 330, 'USD');
+
+        $C = $A->add($B);
+        $this->assertEquals(400, $C->getNett());
+        $this->assertEquals(460, $C->getGross());
+        $this->assertEquals('USD', $C->getCurrencySymbol());
+    }
+
+    /**
+     * @expectedException           LogicException
+     * @expectedExceptionMessage    Can not do operate on different currencies ("USD" and "GBP")
+     */
+    public function testAddingDifferentCurrencies()
+    {
+        $A = new Price(100, 130, 'USD');
+        $B = new Price(300, 330, 'GBP');
+        $A->add($B);
+    }
+
+    /**
+     * @expectedException           LogicException
+     * @expectedExceptionMessage    Nett must not be greater than gross
+     */
+    public function testNettGreaterThanGross()
+    {
+        new Price(100.00, 90.00);
+    }
+
+    public function testNettSameAsGross()
+    {
+        $price = new Price(100.00, 100.00);
+        $this->assertEquals(0, $price->getTax());
+    }
+
+    /**
+     * @expectedException           LogicException
+     * @expectedExceptionMessage    Invalid currency symbol: "PLNG"
+     */
+    public function testInvalidCurrencySymbol()
+    {
+        new Price(100, 200, 'PLNG');
+    }
+
+    public function testSubtractGrossBiggerThanPrice()
+    {
+        $price = new Price(120, 140, 'PLN');
+        $newPrice = $price->subtractGross(150.00);
+
+        $this->assertEquals(0.00, $newPrice->getGross());
+        $this->assertEquals(0.00, $newPrice->getNett());
+        $this->assertEquals(0, $newPrice->getTax());
+    }
+
+    public function testSubstractingGraterPrice()
+    {
+        $smaller = new Price(0.59, 0.72, 'EUR');
+        $bigger = new Price(1.12, 1.32, 'EUR');
+
+        $result = $smaller->subtract($bigger);
+        $this->assertEquals(0.00, $result->getGross());
+        $this->assertEquals(0.00, $result->getNett());
+        $this->assertEquals(0.00, $result->getTax());
+
+    }
+
+    /**
+     * @dataProvider testIsEqualDataProvider
+     */
+    public function testIsEqual($nettA, $grossA, $nettB, $grossB, $expectIsEqual)
+    {
+        $A = new Price($nettA, $grossA);
+        $B = new Price($nettB, $grossB);
+
+        if ($expectIsEqual)
+        {
+            $this->assertTrue($A->isEqual($B));
+            $this->assertTrue($B->isEqual($A));
+            $this->assertEquals($A->getTax(), $B->getTax());
+        }
+        else
+        {
+            $this->assertFalse($A->isEqual($B));
+            $this->assertFalse($B->isEqual($A));
+        }
+    }
+
+    public function testIsEqualDataProvider()
+    {
+        return [
+            [100.00,    123.00,     100.00,     123.00,     true],
+            [100.00,    123.00,     100.01,     123.02,     false],
+            [100.00,    123.00,     100.0014,   123.0021,   true], //fails with precision 4
+        ];
+    }
+}
